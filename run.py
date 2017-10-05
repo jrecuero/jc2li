c20ef09b77d426a3f3ed6952c058e286b8584952
Jose Carlos Recuero Arias <jose.recuero@gmail.com>
2017-10-04 19:18:51 -0700

    Fixes #12 option --execute for running commands from bash

--- run.py
+++ run.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="CLI application launcher.")
     parser.add_argument('--module', '-M', action='store', help='Module with CLI commands')
     parser.add_argument('--echo', action='store_true', help='Echo mode')
-    parser.add_argument('--exception', action='store_true', help='Exception mode')
+    parser.add_argument('--exception', '-X', action='store_true', help='Exception mode')
+    parser.add_argument('--execute', '-E', action='store', help='Execute command')
     args = parser.parse_args()
 
     if args.module is None:
@@ -33,6 +34,12 @@ if __name__ == '__main__':
     except AttributeError:
         print("Module {0} don't have MODULE attribute".format(args.module))
         sys.exit(0)
+
+    if args.execute:
+        print('execute {}'.format(args.execute))
+        cli.exec_user_input(args.execute)
+        sys.exit(0)
+
     try:
         cli_kwargs = {'prompt': 'CLI> '}
         cli_kwargs.update(getattr(mod, mod.MODULE_KWARGS))
